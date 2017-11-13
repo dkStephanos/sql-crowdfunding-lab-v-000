dc4988be946ff944690bd90fc0149934b5adffda
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-13 19:17:05 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,17 +5,17 @@ CREATE TABLE project (
   funding_goal FLOAT,
   start_date TEXT,
   end_date TEXT
-)
+);
 
 CREATE TABLE user (
   id PRIMARY KEY,
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE pledge (
   id PRIMARY KEY,
   amount FLOAT,
   user_id INTEGER,
   project_id INTEGER
-)
+);
